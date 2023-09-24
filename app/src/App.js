17adfdabda24a3ba9792ad96854d0bbc8a271534
DESKTOP-73172LO\szilveszter <nyesteszilv@gmail.com>
2023-09-24 15:54:56 +0200

    Creating the structure of the play screen

--- app/src/App.js
+++ app/src/App.js
@@ -54,6 +54,44 @@ function TopicMenu({ topics, onSelect }) {
   );
 }
 
+function Quiz({ topic, onBackToMenu, data, imgpath }) {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [quizFinished, setQuizFinished] = useState(false);
+  const [score, setScore] = useState(0);
 
+  return (
+    <div className="play">
+ 
+        <div>
+          <div className="header">
+            <h1>{topic} quiz</h1>
+            <p className="question-number">Question {currentQuestion + 1}/{questions.length}</p>
+          </div>
+          {currentQuestionData && (
+            <div>
+             {console.log(`./data/quizzes/${imgpath}/img/${currentQuestionData.image}`)}
+              <img className="pic" src={`./data/quizzes/${imgpath}/${currentQuestionData.image}`} alt={`Question ${currentQuestion + 1}`} />
+            
+            
+              <div className="options">
+                {currentQuestionData.answers.map((answer, ansIndex) => (
+                  <button
+                    className={`btn ${selectedAnswer === ansIndex ? 'selected' : ''}`}
+                    key={ansIndex}
+                    disabled={selectedAnswer !== null}
+                    onClick={() => handleAnswerClick(ansIndex)}
+                  >
+                    {answer}
+                  </button>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+    </div>
+  );
+}
 
 export default App;
